a15e8051fcff94782cb556fec181bdabc59f03f4
calvin <exe985632@gmail.com>
2023-05-12 18:05:26 +0800

    backup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import Editor from "@monaco-editor/react";
+import { load } from "js-yaml";
 
 function App() {
+  const ref = useRef();
+  const [yaml, setYaml] = useState(null);
+
+  useEffect(() => {
+    const getYaml = async () => {
+      const response = await fetch("/amr-config/application.yml");
+      const text = await response.text();
+      const json = await log(text);
+      setYaml(json);
+    };
+    getYaml();
+  }, [yaml]);
+
+  const log = (v) => {
+    return Promise.resolve(v)
+      .then(load)
+      .then((t) => JSON.stringify(t, null, 2));
+  };
+
+  const handleEditorChange = (value, event) => {
+    console.log(event);
+    console.log(value);
+  };
+
+  const onUpdateYml = (v) => {
+    ref.current = v;
+    console.log(v);
+  };
+
+  const handleSubmit = (v) => {
+    console.log(v);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app-container">
+        <div className="editor">
+          <Editor
+            theme="vs-dark"
+            language="yaml"
+            defaultValue={yaml}
+            onChange={handleEditorChange}
+            onMount={(editor) => onUpdateYml(editor)}
+          />
+        </div>
+        <button className="submit-btn" onClick={() => handleSubmit(yaml)}>
+          Update
+        </button>
+      </div>
+    </>
   );
 }
 
